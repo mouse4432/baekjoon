5916e07e3a0f540c04831a3ee0f8cc85815c5adf
mouse4432 <88102469+mouse4432@users.noreply.github.com>
2022-07-19 19:21:24 +0900

    countingWords_1152done

--- CLASS1/countingWords_1152.cpp
+++ CLASS1/countingWords_1152.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <string.h>
+#include <string>
 using namespace std;
 
 int main()
@@ -8,13 +8,17 @@ int main()
     string sentence;
     int cnt = 0;
    
-    getline (cin, sentence);
+    getline(cin, sentence);
+
     if(!sentence.size()==0){
-        for(int i=1; i<sentence.size()-1; i++)
+        for(int i=0; i<sentence.size(); i++)
             if(sentence[i] == ' ')
                 cnt++;
         cnt++;
     }
-    
+    if(sentence[0] == ' ')
+        cnt--;
+    if(sentence[sentence.size()-1] == ' ')
+        cnt--;
     cout << cnt;
 }
